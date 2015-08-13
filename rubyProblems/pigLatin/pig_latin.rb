9bca7804591991bae784fc656d3faead6ca29d2e
Elliot Reed <elliotreed2@gmail.com>
2015-08-13 15:29:29 -0700

    handles multiple words

--- rubyProblems/pigLatin/pig_latin.rb
+++ rubyProblems/pigLatin/pig_latin.rb
@@ -6,17 +6,17 @@ class String
     end.join(' ')
   end
 
-  def pig_latinize_one_word
+  def pig_latinize_one_word(word)
     opening_vowel = /\A[aeiouy]/i
     opening_capital = /\A[A-Z]/
     multiple_consonants = /\A[^aeiouy]*/i
-    if self.match(opening_vowel)
-      self + 'yay'
+    if word.match(opening_vowel)
+      word + 'yay'
     else
-      opening_consonants = self.match(multiple_consonants).to_s
-      rest_of_string = self[(opening_consonants.length)..-1]
+      opening_consonants = word.match(multiple_consonants).to_s
+      rest_of_string = word[(opening_consonants.length)..-1]
       new_string = rest_of_string + opening_consonants + 'ay'
-      self.match(opening_capital) ? new_string.downcase.capitalize : new_string
+      word.match(opening_capital) ? new_string.downcase.capitalize : new_string
     end
   end
 end
