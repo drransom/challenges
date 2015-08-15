13f077938b47ee465d0b05e08234de5c431908ae
Elliot Reed <elliotreed2@gmail.com>
2015-08-14 19:32:04 -0700

    basic linked list specs pass

--- linked_lists/linked_list_problems.rb
+++ linked_lists/linked_list_problems.rb
@@ -1,10 +1,61 @@
-class LinkedList
+
+class DoublyLinkedList
+  attr_reader :length
+
+  def first
+    @first_node ? @first_node.value : nil
+  end
+
+  def last
+    @last_node ? @last_node.value : nil
+  end
+
+  def initialize(items = [])
+    @first_node = nil
+    @last_node = nil
+    @length = 0
+    items.each { |item| push(item) }
+  end
+
+  def empty?
+    length == 0
+  end
+
+  def push(value)
+    node = DLLNode.new(value)
+    node.parent = last_node
+    if @last_node
+      @last_node.child = node
+    else
+      @first_node = node
+    end
+    @last_node = node
+    @length += 1
+    node.value
+  end
+
+  def unshift(value)
+    node = DLLNode.new(value)
+    node.child = @first_node
+    if @first_node
+      @first_node.parent = node
+    else
+      @last_node = node
+    end
+    @first_node = node
+    @length += 1
+    node.value
+  end
+
+private
+  attr_accessor :first_node, :last_node
 end
 
-class LinkedListNode
-  attr_accessor :parent, :value
-  def initialize(value, parent = nil)
+class DLLNode
+  attr_accessor :parent, :child, :value
+  def initialize(value)
     @value = value
-    @parent = parent
+    @parent = nil
+    @child = nil
   end
 end
--- linked_lists/linked_list_spec.rb
+++ linked_lists/linked_list_spec.rb
@@ -1,33 +1,45 @@
 require 'rspec'
 require_relative 'linked_list_problems.rb'
 
-describe LinkedListNode do
+describe DLLNode do
   context 'initialization' do
     it "can be initialized" do
-      expect { LinkedListNode.new(0, nil) }.to_not raise_error
-    end
-
-    it 'does not require a parent to be passed in' do
-      expect { LinkedListNode.new(0) }.to_not raise_error
+      expect { DLLNode.new(0) }.to_not raise_error
     end
   end
 
   context 'setting values' do
     it '#value' do
-      x = LinkedListNode.new(0)
+      x = DLLNode.new(0)
       expect(x.value).to eq(0)
     end
 
     it '#value=' do
-      x = LinkedListNode.new(5)
+      x = DLLNode.new(5)
       x.value = 0
       expect(x.value).to eq(0)
     end
 
   end
 
+  context 'setting child' do
+    subject(:node) { DLLNode.new(0) }
+
+    it '#child=' do
+      child = double("child")
+      expect {node.child = child}.to_not raise_error
+    end
+
+    it '#child' do
+      child = double("child")
+      node.child = child
+      expect(node.child).to eq(child)
+    end
+
+  end
+
   context 'setting parent' do
-    subject(:node) { LinkedListNode.new(0) }
+    subject(:node) { DLLNode.new(0) }
 
     it '#parent=' do
       parent = double("parent")
@@ -39,10 +51,70 @@ describe LinkedListNode do
       node.parent = parent
       expect(node.parent).to eq(parent)
     end
-
   end
 end
 
-describe LinkedList do
-  subject(:list) { LinkedList.new }
+describe DoublyLinkedList do
+
+  context 'initialization' do
+    it 'can be initialized' do
+      expect {DoublyLinkedList.new}.to_not raise_error
+    end
+
+    it 'can take an array as an argument' do
+      expect { DoublyLinkedList.new([1, 2, 3]) }.to_not raise_error
+    end
+
+    it 'adds the array as the arguments' do
+      list = DoublyLinkedList.new([1, 2, 3])
+      expect(list.first).to eq(1)
+      expect(list.last).to eq(3)
+      expect(list.length).to eq(3)
+    end
+  end
+
+  context 'push and unshift' do
+    subject(:list) { DoublyLinkedList.new }
+    describe '#push' do
+      it 'does not raise error' do
+        expect { list.push(5) }.to_not raise_error
+      end
+
+      it 'raises error if nothing is pushed' do
+        expect { list.push() }.to raise_error
+      end
+
+      it 'returns the pushed value' do
+        expect(list.push(5)).to eq(5)
+      end
+
+      it 'pushes adds to the end of the list' do
+        list.push(5)
+        expect(list.last).to eq(5)
+        list.push(3)
+        expect(list.last).to eq(3)
+      end
+    end
+
+    describe '#unshift' do
+      it 'does not raise error' do
+        expect { list.unshift(5) }.to_not raise_error
+      end
+
+      it 'raises error if nothing is unshifted' do
+        expect { list.unshift() }.to raise_error
+      end
+
+      it 'returns the unshifted value' do
+        expect(list.unshift(5)).to eq(5)
+      end
+
+      it 'unshift adds to the end of the list' do
+        list.unshift(5)
+        expect(list.first).to eq(5)
+        list.unshift(3)
+        expect(list.first).to eq(3)
+      end
+    end
+  end
 end
