8e564f40a1bf82605b23e5c945aeffc3d4e3a40e
Elliot Reed <elliotreed2@gmail.com>
2015-08-18 15:03:24 -0700

    write specs for remove_duplicates!

---
+++ linked_lists/linked_list_dups.rb
@@ -0,0 +1,9 @@
+require_relative 'linked_list_problems.rb'
+require 'set'
+
+# class DoublyLinkedList
+#   def remove_duplicates!
+#     seen = Set.new
+#     self.traverse_original_each do |node|
+#       if seen.include?(node.value)
+#
---
+++ linked_lists/linked_lists_dup_spec.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+require_relative 'linked_list_dups.rb'
+
+describe DoublyLinkedList do
+  context 'deleting duplicates' do
+    describe '#remove_duplicates!' do
+      it 'handles one duplicate' do
+        list = DoublyLinkedList.new([1, 1])
+        answer = DoublyLinkedList.new([1])
+        list.remove_duplicates!
+        expect(list).to eq(answer)
+      end
+
+      it 'handles multiple duplicates' do
+        list = DoublyLinkedList.new([1, 1, 1])
+        answer = DoublyLinkedList.new([1])
+        list.remove_duplicates!
+        expect(list).to eq(answer)
+      end
+
+      it 'handles a more complicated case' do
+        list = DoublyLinkedList.new([1, 3, 3, 1, 2, 5, 2])
+        answer = DoublyLinkedList.new([1, 3, 2, 5])
+        list.remove_duplicates!
+        expect(list).to eq(answer)
+      end
+
+      it 'empty list' do
+        list = DoublyLinkedList.new
+        answer = DoublyLinkedList.new
+        list.remove_duplicates!
+        expect(list).to eq(answer)
+      end
+
+      it 'returns the original list' do
+        list = DoublyLinkedList.new([1, 0, 2, 3, 4, 3, 4, 3, 1, 2])
+        expect(list.remove_duplicates!).to eq(list)
+      end
+    end
+  end
+end
