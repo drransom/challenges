98d603ef62bcc150ef49a84d8e855405529f2fa0
Elliot Reed <elliotreed2@gmail.com>
2015-08-24 15:46:07 -0700

    initialize spec passes

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -2,4 +2,11 @@ class SBBST
 end
 
 class BSTNode
+  attr_reader :left_child, :right_child, :parent
+  
+  def initialize(options = {})
+    @left_child = options[:left_child]
+    @right_child = options[:right_child]
+    @parent = options[:parent]
+  end
 end
--- binary_search_tree/binary_search_tree_specs.rb
+++ binary_search_tree/binary_search_tree_specs.rb
@@ -13,9 +13,9 @@ describe BSTNode do
       it 'can take left, right, and parent' do
         options = { left_child: left, right_child: right, parent: parent }
         test_node = BSTNode.new(options)
-        expect test_node.left_child.to eq(left)
-        expect test_node.right_child.to eq(right)
-        expect test_node.parent.to eq(parent)
+        expect(test_node.left_child).to eq(left)
+        expect(test_node.right_child).to eq(right)
+        expect(test_node.parent).to eq(parent)
       end
     end
   end
