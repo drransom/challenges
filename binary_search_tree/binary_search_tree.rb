96dd7c64cf1e2793c0c41ac000b5c9b1afa568d3
Elliot Reed <elliotreed2@gmail.com>
2015-08-24 15:48:36 -0700

    write more initialize specs which pass

--- binary_search_tree/binary_search_tree.rb
+++ binary_search_tree/binary_search_tree.rb
@@ -1,12 +1,14 @@
-class SBBST
+class SelfBalancingBinarySearchTree
 end
 
 class BSTNode
   attr_reader :left_child, :right_child, :parent
-  
+
   def initialize(options = {})
     @left_child = options[:left_child]
     @right_child = options[:right_child]
     @parent = options[:parent]
   end
+
+
 end
--- binary_search_tree/binary_search_tree_specs.rb
+++ binary_search_tree/binary_search_tree_specs.rb
@@ -1,7 +1,7 @@
 require 'rspec'
 require_relative 'binary_search_tree.rb'
 
-describe SBBST do
+describe SelfBalancingBinarySearchTree do
 end
 
 describe BSTNode do
@@ -16,6 +16,18 @@ describe BSTNode do
         expect(test_node.left_child).to eq(left)
         expect(test_node.right_child).to eq(right)
         expect(test_node.parent).to eq(parent)
+      end
+
+      it 'can accept only some options' do
+        options = { left_child: left, parent: parent }
+        test_node = BSTNode.new(options)
+        expect(test_node.left_child).to eq(left)
+        expect(test_node.right_child).to eq(nil)
+        expect(test_node.parent).to eq(parent)
+      end
+
+      it 'requires no options' do
+        expect {BSTNode.new}.to_not raise_error
       end
     end
   end
