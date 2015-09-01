cf70824fd730e8748cd237db8b8abe7b19b554f3
Elliot Reed <elliotreed2@gmail.com>
2015-08-31 17:51:13 -0700

    rename map to sort_by

--- arrayProblems/pythonSort.py
+++ arrayProblems/pythonSort.py
@@ -15,36 +15,36 @@ class SortArray(list):
         # pdb.set_trace()
         start = options['start'] if options.has_key('start') else 0
         end = options['end'] if options.has_key('end') else len(self) - 1
-        if options.has_key('map_function'):
-            map_function = options['map_function']
+        if options.has_key('sort_by'):
+            sort_by = options['sort_by']
         else:
-            map_function = lambda x: x
+            sort_by = lambda x: x
         if len(self) <= 1 or start >= end:
             return
         if start >= end:
             return
         # self.move_pivot_to_start(start, end)
-        mid = self.partition(start, end, map_function)
+        mid = self.partition(start, end, sort_by)
         if start < mid:
-            options = {'start': start, 'end': mid - 1, 'map_function': map_function}
+            options = {'start': start, 'end': mid - 1, 'sort_by': sort_by}
             self.quicksort_in_place(options)
         if mid < end:
-            options = {'start': mid+1, 'end': end, 'map_function': map_function}
+            options = {'start': mid+1, 'end': end, 'sort_by': sort_by}
             self.quicksort_in_place(options)
 
-    def partition(self, start, end, map_function):
+    def partition(self, start, end, sort_by):
         left_index = start + 1
         right_index = end
-        pivot_value = map_function(self[start])
+        pivot_value = sort_by(self[start])
         while right_index >= left_index:
-            if (map_function(self[left_index]) > pivot_value and
-               map_function(self[right_index]) <= pivot_value):
+            if (sort_by(self[left_index]) > pivot_value and
+               sort_by(self[right_index]) <= pivot_value):
                 self.swap(left_index, right_index)
-            if map_function(self[right_index]) > pivot_value:
+            if sort_by(self[right_index]) > pivot_value:
                 right_index -= 1
-            if map_function(self[left_index]) <= pivot_value:
+            if sort_by(self[left_index]) <= pivot_value:
                 left_index += 1
-        if map_function(self[start]) > map_function(self[right_index]):
+        if sort_by(self[start]) > sort_by(self[right_index]):
             self.swap(start, right_index)
         return right_index
 
@@ -72,3 +72,7 @@ class SortArray(list):
             else:
                 new.append(sortArray2.pop(0))
         return new + sortArray1 + sortArray2
+
+x = SortArray([3, 2, 1, 5, 2, -2, -5])
+x.quicksort_in_place({ 'sort_by': lambda x: abs(x)})
+print x
