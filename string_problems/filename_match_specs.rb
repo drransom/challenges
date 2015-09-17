310bcff43eaa9831e1a1f776fd2043dfdec9ed37
Elliot Reed <elliotreed2@gmail.com>
2015-09-17 16:38:51 -0700

    wrote second spec which passes

--- string_problems/filename_match_specs.rb
+++ string_problems/filename_match_specs.rb
@@ -9,5 +9,11 @@ context FileMatcher do
       pattern = 'abcde'
       expect(filenames.pattern_match?(pattern, pattern)).to be_truthy
     end
+
+    it 'does not match an unequal string' do
+      pattern1 = 'abcde'
+      pattern2 = '12345'
+      expect(filenames.pattern_match?(pattern1, pattern2)).to be_falsy
+    end
   end
 end
