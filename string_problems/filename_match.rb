72ea58609a6ff6cb2494b9cc54ee3caf7be883f3
Elliot Reed <elliotreed2@gmail.com>
2015-09-17 20:13:12 -0700

    refactor to move data into custom object

--- string_problems/filename_match.rb
+++ string_problems/filename_match.rb
@@ -2,39 +2,60 @@ require 'byebug'
 
 class FileMatcher
 
-  def match(pattern, filenames)
-    filenames.select { |filename| pattern_match?(pattern, filename)}
+  def match(filenames, pattern)
+    filenames.select { |filename| pattern_match?(filename, pattern) }
   end
 
-  def pattern_match?(pattern, filename, options = {})
-    pattern_index = options[:pattern_index] || 0
-    file_index = options[:file_index] || 0
-    return true if pattern[pattern_index..-1] == filename[file_index..-1]
-    pattern_index.upto(pattern.length-1) do |idx|
-      return false if file_index > filename.length
+  def pattern_match?(filename, pattern)
+    check_pattern_match?(FilenameMatchData.new(filename, pattern))
+  end
+
+  def check_pattern_match?(matchData, options = {})
+    pattern_idx = options[:pattern_idx] || 0
+    file_idx = options[:file_idx] || 0
+    pattern = matchData.pattern
+    filename = matchData.filename
+    return true if matchData.match_remaining?(file_idx, pattern_idx)
+    pattern_idx.upto(pattern.length-1) do |idx|
+      return false if file_idx > filename.length
       case pattern[idx]
       when '?'
-        return match_with_wildcard?(pattern, filename, idx, file_index)
+        return match_with_wildcard?(matchData, idx, file_idx)
       when '*'
-        return match_with_all_wildcard?(pattern, filename, idx, file_index)
+        return match_all_with_wildcard?(matchData, idx, file_idx)
       else
-        return false unless pattern[idx] == filename[file_index]
-        file_index += 1
+        return false unless matchData.match?(file_idx, idx)
+        file_idx += 1
       end
     end
-    file_index == filename.length
+    file_idx == filename.length
   end
 
-  def match_with_wildcard?(pattern, filename, pattern_index, file_index)
-    new_idx = pattern_index + 1
-    pattern_match?(pattern, filename, {pattern_index: new_idx, file_index: file_index}) ||
-    pattern_match?(pattern, filename, {pattern_index: new_idx, file_index: file_index + 1})
+  def match_with_wildcard?(matchData, pattern_idx, file_idx)
+    new_idx = pattern_idx + 1
+    match_all_with_wildcard?(matchData, pattern_idx, file_idx, file_idx + 1)
   end
 
-  def match_with_all_wildcard?(pattern, filename, pattern_index, file_index, max = nil)
-    file_index.upto(max || filename.length) do |idx|
-      return true if pattern_match?(pattern, filename, {pattern_index: pattern_index + 1, file_index: idx})
+  def match_all_with_wildcard?(matchData, pattern_idx, file_idx, max = nil)
+    file_idx.upto(max || matchData.filename.length) do |idx|
+      return true if check_pattern_match?(matchData, {pattern_idx: pattern_idx + 1, file_idx: idx})
     end
     false
+  end
+
+  class FilenameMatchData
+    attr_reader :filename, :pattern
+    def initialize(filename, pattern)
+      @filename = filename
+      @pattern = pattern
+    end
+
+    def match?(file_idx, pattern_idx)
+      @filename[file_idx] == @pattern[pattern_idx]
+    end
+
+    def match_remaining?(file_idx, pattern_idx)
+      @filename[file_idx..-1] == @pattern[pattern_idx..-1]
+    end
   end
 end
--- string_problems/filename_match_specs.rb
+++ string_problems/filename_match_specs.rb
@@ -13,139 +13,139 @@ context FileMatcher do
     it 'does not match an unequal string' do
       pattern = 'abcde'
       string = '12345'
-      expect(filenames.pattern_match?(pattern, string)).to be_falsy
+      expect(filenames.pattern_match?(string, pattern)).to be_falsy
     end
 
     it 'matches a string with a one-character wildcard' do
       pattern = 'ab?de'
       string = 'abcde'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'wildcard can be no characters' do
       pattern = 'ab?de'
       string = 'abde'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'one-character wildcard at start' do
       pattern = '?bcde'
       string = 'abcde'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'zero-character wildcard at start' do
       pattern = '?bcde'
       string = 'bcde'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'one-character wildcard at end' do
       pattern = 'abcd?'
       string = 'abcde'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'zero-character wildcard at end' do
       pattern = 'abcd?'
       string = 'abcd'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'question mark in string is not wild' do
       pattern = 'abcde'
       string = 'ab?de'
-      expect(filenames.pattern_match?(pattern, string)).to be_falsy
+      expect(filenames.pattern_match?(string, pattern)).to be_falsy
     end
 
     it 'question mark matches an empty string' do
       pattern = '?'
       string = ''
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'multiple question marks in a row' do
       pattern = 'ab??e'
       string = 'abe'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'multiple opening question marks' do
       pattern = '??bcde'
       string = 'abcde'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'has question mark but does not match' do
       pattern = 'ab?de'
       string = 'abcee'
-      expect(filenames.pattern_match?(pattern, string)).to be_falsy
+      expect(filenames.pattern_match?(string, pattern)).to be_falsy
     end
 
     it 'unlimited character wildcard works with one character' do
       pattern = '*'
       string = 'abc'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'multiple chars at start of string' do
       pattern = '*c'
       string = 'abc'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'no chars at start of string' do
       pattern = '*abc'
       string = 'abc'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'no chars in middle of string' do
       pattern = 'abc*def'
       string = 'abcdef'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'multiple chars in middle of string' do
       pattern = 'abc*def'
       string = 'abcasdfhgrwerqasdfdef'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
-    
+
     it 'no chars at end of string' do
       pattern = 'abc*'
       string = 'abc'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'multiple chars at end of string' do
       pattern = 'abc*'
       string = 'abcdef'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'no match' do
       pattern = 'abc*def'
       string = 'abcasdfdefh'
-      expect(filenames.pattern_match?(pattern, string)).to be_falsy
+      expect(filenames.pattern_match?(string, pattern)).to be_falsy
     end
 
     it 'multiple wildcards in a row' do
       pattern = 'abc**def'
       string = 'abcasdfasdfdef'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'wildcards at different points in a string' do
       pattern = 'a*b*c'
       string = 'aasdfbasdfasdfc'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
 
     it 'different types of wildcards' do
       pattern = 'a*b??e'
       string = 'adfgdfgdfgbfe'
-      expect(filenames.pattern_match?(pattern, string)).to be_truthy
+      expect(filenames.pattern_match?(string, pattern)).to be_truthy
     end
   end
 end
