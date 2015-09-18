2e2318db2e898926293148d6947a9cc5b4ae725c
Elliot Reed <elliotreed2@gmail.com>
2015-09-18 09:01:30 -0700

    moved logic into custom class

--- string_problems/filename_match.rb
+++ string_problems/filename_match.rb
@@ -7,40 +7,7 @@ class FileMatcher
   end
 
   def pattern_match?(filename, pattern)
-    check_pattern_match?(FilenameMatchData.new(filename, pattern))
-  end
-
-  def check_pattern_match?(matchData, options = {})
-    pattern_idx = options[:pattern_idx] || 0
-    file_idx = options[:file_idx] || 0
-    pattern = matchData.pattern
-    filename = matchData.filename
-    return true if matchData.match_remaining?(file_idx, pattern_idx)
-    pattern_idx.upto(pattern.length-1) do |idx|
-      return false if file_idx > filename.length
-      case pattern[idx]
-      when '?'
-        return match_with_wildcard?(matchData, idx, file_idx)
-      when '*'
-        return match_all_with_wildcard?(matchData, idx, file_idx)
-      else
-        return false unless matchData.match?(file_idx, idx)
-        file_idx += 1
-      end
-    end
-    file_idx == filename.length
-  end
-
-  def match_with_wildcard?(matchData, pattern_idx, file_idx)
-    new_idx = pattern_idx + 1
-    match_all_with_wildcard?(matchData, pattern_idx, file_idx, file_idx + 1)
-  end
-
-  def match_all_with_wildcard?(matchData, pattern_idx, file_idx, max = nil)
-    file_idx.upto(max || matchData.filename.length) do |idx|
-      return true if check_pattern_match?(matchData, {pattern_idx: pattern_idx + 1, file_idx: idx})
-    end
-    false
+    FilenameMatchData.new(filename, pattern).pattern_match?
   end
 
   class FilenameMatchData
@@ -56,6 +23,37 @@ class FileMatcher
 
     def match_remaining?(file_idx, pattern_idx)
       @filename[file_idx..-1] == @pattern[pattern_idx..-1]
+    end
+
+    def pattern_match?(options = {})
+      pattern_idx = options[:pattern_idx] || 0
+      file_idx = options[:file_idx] || 0
+      return true if match_remaining?(file_idx, pattern_idx)
+      pattern_idx.upto(pattern.length-1) do |idx|
+        return false if file_idx > filename.length
+        case pattern[idx]
+        when '?'
+          return match_with_wildcard?(idx, file_idx)
+        when '*'
+          return match_all_with_wildcard?(idx, file_idx)
+        else
+          return false unless match?(file_idx, idx)
+          file_idx += 1
+        end
+      end
+      file_idx == filename.length
+    end
+
+    def match_with_wildcard?(pattern_idx, file_idx)
+      new_idx = pattern_idx + 1
+      match_all_with_wildcard?(pattern_idx, file_idx, file_idx + 1)
+    end
+
+    def match_all_with_wildcard?(pattern_idx, file_idx, max = nil)
+      file_idx.upto(max || filename.length) do |idx|
+        return true if pattern_match?({pattern_idx: pattern_idx + 1, file_idx: idx})
+      end
+      false
     end
   end
 end
