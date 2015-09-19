3802c343ff9edc6a3a21710f7ebac8864d42a413
Elliot Reed <elliotreed2@gmail.com>
2015-09-19 10:36:26 -0700

    write kmp search specs

--- string_problems/shortest_palindrome_specs.rb
+++ string_problems/shortest_palindrome_specs.rb
@@ -17,4 +17,33 @@ describe '#shortest_palindrome' do
   it 'not already a palindrome' do
     expect(shortest_palindrome('ba')).to eq('aba')
   end
+
+end
+
+context String do
+  describe '#kmp_search' do
+    it 'empty string' do
+      expect(''.kmp_search('a')).to be_falsy
+    end
+
+    it 'simple case' do
+      expect('abc'.kmp_search('bc')).to eq(1)
+    end
+
+    it 'string appears multiple times' do
+      expect('abcdbcd'.kmp_search('bcd')).to eq(1)
+    end
+
+    it 'search for empty string' do
+      expect('abc'.kmp_search('')).to eq(0)
+    end
+
+    it 'searched string equals entire string' do
+      expect('abcdefg'.kmp_search('abcdefg')).to eq(0)
+    end
+
+    it 'searched string is not present' do
+      expect('abcdefg'.kmp_search('abdeg')).to be_falsy
+    end
+  end
 end
