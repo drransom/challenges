b4ff4f4e38b56c9377a8d951f7f6509d40001b20
Elliot Reed <elliotreed2@gmail.com>
2015-09-19 10:12:11 -0700

    write specs for shortest palindrome function

--- string_problems/shortest_palindrome_specs.rb
+++ string_problems/shortest_palindrome_specs.rb
@@ -1,2 +1,20 @@
 require 'rspec'
 require_relative 'shortest_palindrome'
+
+describe '#shortest_palindrome' do
+  it 'empty string' do
+    expect(shortest_palindrome('')).to eq('')
+  end
+
+  it 'one character string' do
+    expect(shortest_palindrome('a')).to eq('a')
+  end
+
+  it 'three character palindrome' do
+    expect(shortest_palindrome('aba')).to eq('aba')
+  end
+
+  it 'not already a palindrome' do
+    expect(shortest_palindrome('ba')).to eq('aba')
+  end
+end
