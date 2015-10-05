21ecd3699647c6e656d277e908121310661f3fff
Elliot Reed <elliotreed2@gmail.com>
2015-10-05 11:24:17 -0700

    refactor ugly numbers

--- rubyProblems/ugly_numbers.rb
+++ rubyProblems/ugly_numbers.rb
@@ -1,26 +1,50 @@
 require 'set'
 
 def nth_ugly_number(n)
-    return nil if n <= 0
-    visited = Set.new
-    visited << 1
-    uglies = [1];
-
-    add_number = Proc.new do |number|
-      unless visited.include?(number)
-        uglies << number
-        visited << number
-      end
-    end
-
-    current_index = 0
-    while uglies.length < n do
-      current_num = uglies[current_index]
-      [2, 3, 5].each { |num| add_number.call(current_num * num) }
-      current_index += 1
-    end
-    puts uglies.to_s
-    uglies.sort[n-1]
+  return nil if n <= 0
+  finder = UglyNumberFinder.new
+  finder.find_nth_ugly(n)
 end
 
-puts nth_ugly_number(500)
+class UglyNumberFinder
+  def initialize(indices = {2: 0, 3: 0, 5: 0})
+    @visited = Set.new([1])
+    @uglies = [1]
+    @indices = indices
+  end
+
+  def increment_indices
+    @indices.each do |value, index|
+      increment_index(value) while @visited.include?(uglies[index] * value)
+    end
+
+  def increment_index(value)
+    @indices[value] += 1
+  end
+
+  def find_next_ugly
+    increment_indices
+    increments = []
+    @indices.each do |num, index|
+      increments << [num * @uglies[index]]
+    end
+    return increments.min
+  end
+
+  def update_uglies!
+    next_ugly = find_next_ugly
+    @uglies << next_ugly
+    @visited << next_ugly
+  end
+
+  def find_nth_ugly(n)
+    while @uglies.length < n do
+      update_uglies!
+    end
+    @uglies[n-1]
+  end
+end
+
+
+
+puts nth_ugly_number(150)
