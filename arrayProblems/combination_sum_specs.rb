e4cc330b7fff105179eba27e1dfce10d23f63167
Elliot Reed <elliotreed2@gmail.com>
2015-10-08 18:21:03 -0700

    start combination sum problem

---
+++ arrayProblems/combination_sum_specs.rb
@@ -0,0 +1,26 @@
+require 'rspec'
+require_relative 'combination_sum'
+
+describe '#combination_sum' do
+  it 'no candidates' do
+    expect(combination_sum([], 0)).to eq([])
+  end
+
+  it 'simple case' do
+    expect(combination_sum([1], 1)).to eq([1])
+  end
+
+  it 'another simple case' do
+    answer = [[1, 1, 1, 1], [1, 1, 2], [2, 2]].sort
+    expect(combination_sum([1, 2], 4).sort).to eq(answer)
+  end
+
+  it 'nothing adds up' do
+    expect(combination_sum([2, 4, 6], 9)).to eq([])
+  end
+
+  it 'duplicates not produced' do
+    answer = [[1, 1], [2]].sort
+    expect(combination_sum([1, 1, 1, 2], 2).sort).to eq(answer)
+  end
+end
