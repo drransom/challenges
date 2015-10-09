80e25b21c3df372afdc20912485552c7a9bd0b54
Elliot Reed <elliotreed2@gmail.com>
2015-10-08 19:27:03 -0700

    problem 2 passes all specs

--- arrayProblems/combination_sum.rb
+++ arrayProblems/combination_sum.rb
@@ -1,3 +1,4 @@
+require 'byebug'
 def combination_sum(candidates, target)
   CombinationFinder.new(candidates).find_combinations(target)
 end
@@ -16,15 +17,12 @@ class CombinationFinder
 
   def find_combinations(target, end_index = nil )
     end_index ||= candidates.length - 1
-    while end_index >= 0 && candidates[end_index] > target
-      end_index -= 1
-    end
     current_answer = []
     while end_index >= 0
       current = candidates[end_index]
       if target == current
         current_answer << [current]
-      else
+      elsif target > current
         new_combinations = find_combinations(target - current, end_index)
         new_combinations.each { |combination| combination << current }
         current_answer += new_combinations
@@ -34,8 +32,22 @@ class CombinationFinder
     current_answer
   end
 
-  def find_combinations_no_repeats(target)
-    nil
+  def find_combinations_no_repeats(target, end_index = nil)
+    end_index ||= candidates.length
+    end_index -= 1
+    current_answer = []
+    while end_index >= 0
+      current = candidates[end_index]
+      if target == current
+        current_answer << [current]
+      elsif target > current
+        new_combinations = find_combinations_no_repeats(target - current, end_index)
+        new_combinations.each { |combination| combination << current }
+        current_answer += new_combinations
+      end
+      end_index -= 1
+    end
+    current_answer
   end
 
 end
