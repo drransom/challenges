504c7df628a2514da241d6551b041539d6c2d3e0
Elliot Reed <elliotreed2@gmail.com>
2015-10-09 16:27:45 -0700

    refactor with additional object, all specs pass

--- arrayProblems/find_duplicate.rb
+++ arrayProblems/find_duplicate.rb
@@ -4,11 +4,44 @@ def find_duplicate(nums)
 end
 
 class DuplicateFinder
-  attr_reader :nums, :sum, :excess
+  attr_reader :nums
   def initialize(nums)
     @nums = nums
-    @sum = nums.inject(0, :+)
-    @excess = sum - expected_sum_upto_n(nums.length - 1)
+  end
+
+
+  def find_duplicate
+    base = check_bases_cases.call
+    return base if base
+
+    find_duplicate_without_base
+
+  end
+
+  def find_duplicate_without_base
+    RangeStatusChecker.new(nums).find_duplicate
+  end
+
+  def check_bases_cases
+    lambda do
+      return 0 if nums.length == 0
+      return 1 if nums.length == 1
+    end
+  end
+end
+
+class RangeStatusChecker
+  attr_accessor :nums, :low, :high, :mid
+
+  def initialize(nums)
+    @nums = nums
+    @low = 1
+    @high = nums.length - 1
+    update_mid
+  end
+
+  def update_mid
+    @mid = ( high + low ) / 2
   end
 
   def actual_sum_between(low, high)
@@ -24,57 +57,61 @@ class DuplicateFinder
   end
 
   def find_duplicate
-    base = check_bases_cases.call
-    return base if base
-
-    find_duplicate_without_base
-
-  end
-
-  def find_duplicate_without_base
-    low = 1
-    high = nums.length - 1
-    mid = (low + high) / 2
-
-    until low == high do
-      actual_low = actual_sum_between(low, mid)
-      actual_high = actual_sum_between(mid, high)
-      expected_low = expected_sum_between(low, mid)
-      expected_high = expected_sum_between(mid, high)
-      if actual_low == expected_low
-        #duplicate cannot be in this range
-        low = mid
-      elsif actual_high == expected_high
-        #duplicate cannot be in top of range
-        high = mid
-      elsif actual_low < expected_low
-        #low value is replacing higher value within low range,
-        #or high range value is replacing values in low range
-        if actual_high < expected_high
-          #number from low range in high range
-          high = mid
-        else
-          #number from range in low range
-          low = mid
-        end
-      else
-        #low value is replacing high vaue within low range
-        high = mid
-      end
-
-      if high - low == 1
-        if is_duplicate?(high)
-          low = high
-        elsif is_duplicate?(low)
-          high = low
-        else
-          raise StandardError.new('converged to wrong answer')
-        end
-      end
-
-      mid = (low + high) / 2
+    until low == high
+      update_range
+      check_convergence_off_one
+      update_mid
     end
     mid
+  end
+
+  def check_convergence_off_one
+    if high - low == 1
+      if is_duplicate?(high)
+        @low = high
+      elsif is_duplicate?(low)
+        @high = low
+      else
+        raise StandardError.new('converged to wrong answer')
+      end
+    end
+  end
+
+
+  def update_range
+    if determine_range == :low
+      @high = mid
+    else
+      @low = mid
+    end
+  end
+
+  def determine_range
+    actual_low = actual_sum_between(low, mid)
+    actual_high = actual_sum_between(mid, high)
+    expected_low = expected_sum_between(low, mid)
+    expected_high = expected_sum_between(mid, high)
+
+    if actual_low == expected_low
+      #duplicate cannot be in this range
+      :high
+    elsif actual_high == expected_high
+      #duplicate cannot be in top of range
+      :low
+    elsif actual_low < expected_low
+      #low value is replacing higher value within low range,
+      #or high range value is replacing values in low range
+      if actual_high < expected_high
+        #number from low range in high range
+        :low
+      else
+        #number from high range in low range
+        :high
+      end
+    else
+      #low value is replacing high vaue within low range
+      :low
+    end
   end
 
   def is_duplicate?(num)
@@ -88,11 +125,6 @@ class DuplicateFinder
     false
   end
 
-
-  def check_bases_cases
-    lambda do
-      return 0 if nums.length == 0
-      return 1 if nums.length == 1
-    end
-  end
 end
+
+puts find_duplicate([1, 2, 2])
